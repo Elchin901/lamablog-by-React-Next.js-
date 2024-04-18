9860292d62e922a70c12fd8d321075c2bacdcebc
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-04-18 15:35:23 +0200

    ok

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="/">Hello</Link>
+    </div>
+    
+  );
+}

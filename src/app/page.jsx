735491efc547fb1a3fc7961665c70b29aff19b30
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-04-23 22:08:40 +0200

    ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "./page.module.css";
-import { Featured } from "@/components/featured/Featured";
+import  Featured  from "@/components/featured/Featured";
 import { CategoryList } from "@/components/categoryList/CategoryList";
 import { CardList } from "@/components/cardList/CardList";
 import { Menu } from "@/components/Menu/Menu";
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import styles from "./featured.module.css"
+import Image from 'next/image'
 
-export const Featured = () => {
+const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, lama dev her!</b> Discover my stories and creative ideas
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill/>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>lorem ipsum dolor sit amet mumet</h1>
+          <p className={styles.postDesc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Explicabo, fuga sunt placeat iste nisi unde assumenda itaque eos laborum in. 
+            Laboriosam, aperiam amet ullam error harum excepturi a natus odit.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;

2688071715acede508f933caadf61c0709a213de
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-04-25 13:36:13 +0200

    ok

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import style from "./menu.module.css"
 
 export const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className={style.container}>Menu</div>
   )
 }
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Pagination } from '../pagination/Pagination'
 import styles from "./cardList.module.css"
+import Card from '../card/Card'
+import Image from 'next/image'
 
 export const CardList = () => {
   return (
     <div className={styles.container}>
-        Cardlist
+        <h1 className={styles.title}>Recent Posts</h1>
+        <div className={styles.posts}>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
         <Pagination/>
     </div>
   )

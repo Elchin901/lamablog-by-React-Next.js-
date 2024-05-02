e1e2bf7d462ad30c33562e2ec21c3db704787cad
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-05-02 11:38:01 +0200

    ok

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -8,7 +8,7 @@ import { ThemeContext } from '@/context/ThemeContext';
 export const ThemeToggle = () => {
   const {toggle, theme} = useContext(ThemeContext);
 
-  console.log(theme)
+ 
 
   return (
     <div className={styles.container} onClick={toggle}

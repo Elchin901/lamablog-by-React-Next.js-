e302a710d8e139e9b686b1dba2d8f6a6bfdf6c7e
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2024-05-07 23:08:55 +0200

    ok

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -4,7 +4,21 @@ import styles from "./cardList.module.css"
 import Card from '../card/Card'
 import Image from 'next/image'
 
-export const CardList = () => {
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+export const CardList =async ({page}) => {
+
+  const data =  await getData(page)
   return (
     <div className={styles.container}>
         <h1 className={styles.title}>Recent Posts</h1>
